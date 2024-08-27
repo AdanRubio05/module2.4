5ffdd9a2911e3b66bed7438273f7ae520e1a386e
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 17:38:46 -0500

    back to original set up

--- appz.js
+++ appz.js
@@ -3,8 +3,3 @@ var name = "Another Console Message?!"; // This is a global variable, no?
 document.write('This should be a headline!');
 
 console.log(name);
-
-const person = {firstName: 'Adan', lastName: 'Rubio', age:21, eyeColor: 'Unknown'};
-
-document.getElementById("info").innerHTML =
-person.firstName + lastName + 'is' + person.age + 'y.o';
