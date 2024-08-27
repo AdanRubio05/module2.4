ab6982e01302c05ce8f810289783f3c016c00539
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 16:57:23 -0500

    Defined Console Value
    console was given the value of x

--- appz.js
+++ appz.js
@@ -1,2 +1,4 @@
-console.log('Another Console Log!?');
-document.write('This should be a headline!')
+console.log(x);
+document.write('This should be a headline!');
+var x = 7; //this is a global variable, no?
+
