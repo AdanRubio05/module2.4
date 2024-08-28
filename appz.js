c26b307009f1dbdc656894f85eeed3fad9586376
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 20:42:17 -0500

    tinkering

--- appz.js
+++ appz.js
@@ -1,11 +1,13 @@
 var total = 3; // This is a global variable, no?
 
-console.log(total);
+console.log(total); 
 
 document.write('What is 9 / 3? (answer in console log)');
 
-let x = 7 ;
+let x = 7 ; // These three are block variables, yes?
 let y = 2 ;
 let z = x * y ;
 
 document.getElementById('equation').innerHTML = x + " " + "multiplied by" + " " + y + " " + "makes the product of:" + " " + z ;
+
+ 
