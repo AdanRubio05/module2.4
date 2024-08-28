5710ef3aa46913173c5fa162c0ec79a77600b068
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 19:35:25 -0500

    tinkering

--- appz.js
+++ appz.js
@@ -1,5 +1,15 @@
-var name = "Another Console Message?!"; // This is a global variable, no?
+var total = 3; // This is a global variable, no?
 
-document.write('This should be a headline!');
+console.log(total);
 
-console.log(name);
+document.write('This is my math equation below:');
+
+/*let x = 7;
+let y = 2;
+let z = x * y;
+document.getElementById("equation").innerHTML =
+        "The product of z comes to:" + z + "!";*/
+const felines = ['Tiger', 'Lion', 'Panther']
+
+
+
