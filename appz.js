4e8267fa99856236b18b60bc1ed078cc3961b7d0
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 19:36:35 -0500

    back to start

--- appz.js
+++ appz.js
@@ -4,12 +4,3 @@ console.log(total);
 
 document.write('This is my math equation below:');
 
-/*let x = 7;
-let y = 2;
-let z = x * y;
-document.getElementById("equation").innerHTML =
-        "The product of z comes to:" + z + "!";*/
-const felines = ['Tiger', 'Lion', 'Panther']
-
-
-
