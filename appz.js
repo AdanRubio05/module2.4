5a4b4db071c84e30350a0cf06ca16531cfc309ac
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 20:10:10 -0500

    more tinkering

--- appz.js
+++ appz.js
@@ -2,5 +2,5 @@ var total = 3; // This is a global variable, no?
 
 console.log(total);
 
-document.write('This is my math equation below:');
+document.write('Check out the Math in the Console Logs!');
 
