2ffaf066e4d3cb265a6bf117123fa573279066f6
AdanRubio05 <adamrubi24@gmail.com>
2024-08-27 20:27:47 -0500

    Variables now displaying on HTML doc
    I don't know how I did it but the JS variables I entered now appear on the HTML doc. What I did: I first and foremost created an ID name on the HTML page in VS, then I created and defined variables in the JS page. finally I put down the doc.elementID prompt.

--- appz.js
+++ appz.js
@@ -2,5 +2,10 @@ var total = 3; // This is a global variable, no?
 
 console.log(total);
 
-document.write('Check out the Math in the Console Logs!');
+document.write('What is 9 / 3? (answer in console log)');
 
+let x = 7 ;
+let y = 2 ;
+let z = x * y ;
+
+document.getElementById('equation').innerHTML = x + " " + "multiplied by" + " " + y + " " + "makes the product of:" + " " + z ;
